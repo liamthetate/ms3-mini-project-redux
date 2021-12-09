e4953c48a6a2a09c02fe1314980e57d445dc52b3
Liam Tate <mrtea@protonmail.com>
2021-12-09 10:25:42 +0000

    tooltips etc

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
     $('.sidenav').sidenav({edge: 'right'});
   });
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+
+  $(document).ready(function(){
+    $('.tooltipped').tooltip();
+  });
